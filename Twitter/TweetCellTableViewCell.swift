86c3dce956135c85b9feb03c4ad2a55aef5460f2
harmony222 <scarleth@oregonstate.edu>
2021-03-12 09:07:23 -0800

    add gif to readme, comment out media image code for now (layout not working correctly yet)

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,10 +13,11 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
-    @IBOutlet weak var tweetStackView: UIStackView!
-    @IBOutlet weak var tweetCellContentView: UIView!
     
-    @IBOutlet weak var mediaImageView: UIImageView!
+//    @IBOutlet weak var tweetStackView: UIStackView!
+//    @IBOutlet weak var tweetCellContentView: UIView!
+//    @IBOutlet weak var mediaImageView: UIImageView!
+    
     @IBOutlet weak var retweetButton: UIButton!
     @IBOutlet weak var favButton: UIButton!
     
